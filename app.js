1bd6e1e279b1ac333defc9fe6e453e508cf8c9a4
Asad2442 <asadullahjan759@gmail.com>
2024-12-05 19:40:13 +0000

    Initial Node.js app

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from GitHub Actions and Ansible!');
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
